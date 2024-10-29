8944f1b56fdbdbc418405f3931b95ad356d11277
Mark-laurence <mvino626@gmai.com>
2024-10-29 20:40:36 +0800

    crud commit

---
+++ read.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+include 'db.php';
+
+$sql = "SELECT * FROM crud";
+$result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "<table border='1'>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Action</th>
+                </tr>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>
+                    <td>" . $row['ID'] . "</td>
+                    <td>" . $row['Name'] . "</td>
+                    <td>" . $row['Email'] . "</td>
+                    <td>
+                        <a href='update.php?id=" . $row['ID'] . "'>Edit</a> | 
+                        <a href='delete.php?id=" . $row['ID'] . "' onclick='return confirm(\"Are you sure?\")'>Delete</a>
+                    </td>
+                </tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "No records found.";
+}
+$conn->close();
+?>
+
+</body>
+</html>
